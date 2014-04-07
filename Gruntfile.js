1bb4c0302cdc5da20f499f9503152ff2a9b5dfd7
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-04-06 19:43:45 -0700

    updating styles to stylus precompiler, tweeking some older styles, changing up logo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,65 @@
 module.exports = function ( grunt ) {
+  var pkg = require('./package.json'),  
+    fs = require('fs');
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     nodewebkit: {
       options: {
         build_dir: './builds', 
-        linux64: false,
-        app_name : "Gist About",
+        linux64: true,
+        app_name : "Pastila",
         app_version : "<% pkg.version %>"
       },
       src: ['./app/**/*'] 
+    },
+    watch: {
+      styles: {
+        files: ['assets/stylus/**/*.styl'],
+        tasks: ['stylus']
+      },
+      pkg : {
+        files : ['./package.json'],
+        task : ['syncPackage']
+      }
+    },
+    stylus: {
+      compile: {
+        options: {
+          paths: ['assets/stylus/ui', 'assets/stylus'],
+          urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
+          use: [
+            require('nib') // use stylus plugin at compile time
+          ]
+        },
+        files: {
+          'app/css/main.css': 'assets/stylus/main.styl', // 1:1 compile
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('syncPackage', function ( ) {
+    var appPkg = require('./app/package.json'),
+        update;
+    if( pkg.version !== appPkg.version ) {
+      update = true;
+      appPkg.version = pkg.version;
+    }
+
+    if( pkg.name !== appPkg.name ) {
+      update = true;
+      appPkg.name = pkg.name;
+    }
+
+    if ( update ) {
+      console.log('Updating app/package.json');
+      fs.writeFileSync('./app/package.json', JSON.stringify(appPkg, null, '\t'));
+    }
+  });
+
+  grunt.registerTask('default', ['stylus', 'syncPackage', 'watch']);
 };
--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var spawn = require('child_process').spawn;
 	nodewebkit = spawn('node_modules/.bin/nodewebkit', ['app']),
-	myth = spawn('node_modules/.bin/myth', 
-		['-watch', 'app/css/myth/main.css', 'app/css/main.css']);
+  grunt = spawn('grunt');
 
 nodewebkit.on('end', function(){
 	throw "Node Webkit Exitted";	
 });
 
-myth.on('end', function(){
-	throw "Myth Exitted";	
-});
-
 // let node webkit be vocal
 nodewebkit.stdout.pipe( process.stdout );
 nodewebkit.stderr.pipe( process.stdout );
+grunt.stdout.pipe( process.stdout );
+grunt.stderr.pipe( process.stdout );
